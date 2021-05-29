6847dbf8280c7d1846283f1cb12bf129510e9401
Max Melcher <max@melcher.it>
2021-05-29 23:46:41 +0200

    removing the enhanced security config

--- sources/SetupVSServer.ps1
+++ sources/SetupVSServer.ps1
@@ -15,11 +15,11 @@ $proc | Wait-Process
 
 
 # Disable Internet Explorer Enhanced Security Configuration
-$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
-$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
-Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
-Stop-Process -Name Explorer -Force
+#$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+#$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+#Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
+#Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
+#Stop-Process -Name Explorer -Force
 
 # Download eShopOnWeb to c:\eShopOnWeb and extract contents
 $zipFileeShopTemp = [System.IO.Path]::GetTempPath().ToString() + "eShopOnWeb-master.zip"
