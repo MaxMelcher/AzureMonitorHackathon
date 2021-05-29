207f7000101b778082b10258c4ec2aa9d7bc68bb
Max Melcher <max@melcher.it>
2021-05-30 00:57:26 +0200

    removing the security

--- sources/SetupWebServers.ps1
+++ sources/SetupWebServers.ps1
@@ -49,11 +49,11 @@ $proc | Wait-Process
 
 
 # Disable Internet Explorer Enhanced Security Configuration
-$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
-$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
-Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
-Stop-Process -Name Explorer -Force
+#$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+#$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+#Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
+#Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
+#Stop-Process -Name Explorer -Force
 
 # Copy eShoponWeb from Published Share and restart IIS
 $SharePath = '\\'+$VSServerName+'\eShopPub'
